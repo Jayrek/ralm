941ee4e8f8049639a2d35c7578ab7ca8c267f2ae
Jayrek <jrektabasa@gmail.com>
2025-03-10 08:00:58 +0800

    add initial screens, and sub directories

---
+++ lib/feature/know_yourself/screen/know_yourself_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KnowYourSelfScreen extends StatelessWidget {
+  const KnowYourSelfScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Know YourSelf')));
+  }
+}
---
+++ lib/feature/signs/screen/signs_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignsScreen extends StatelessWidget {
+  const SignsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Signs')));
+  }
+}
